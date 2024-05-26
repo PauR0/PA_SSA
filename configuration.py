f0ad61573b8e000c00cb11e395d2e1edd8b1d8f4
Pau Romero <pau.romero@uv.es>
2024-05-26 20:25:44 +0200

    Added a configuration file in python lang

---
+++ configuration.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+#####
+# Script with general stuff
+#####
+
+
+import os
+
+input_dir = "/Users/pau/Aortes/KCL/malak_PAs/"
+input_suffix = "_SSA_surf.vtk"
+n_cores = 4
+input_fnames = sorted([f for f in os.listdir(input_dir) if f.endswith(input_suffix)])[:4]
+
+cohort_dir = "/Users/pau/Aortes/KCL/PA_SSA"
+os.makedirs(cohort_dir, exist_ok=True) #Create in case it does not exist.
+
+case_directories = [cd for cd in os.listdir(cohort_dir) if os.path.exists(os.path.join(cohort_dir, cd, 'Meshes', 'mesh_input.vtk'))][:4]
+
+def id_parser(s):
+    return s.replace(input_suffix, "") #Extract the id from filename
+
+
+hierarchy = {
+    "PT"  : {"parent" : None,  "children" : {"LPA"}},
+    "LPA" : {"parent" : "PT",  "children" : {"RPA"}},
+    "RPA" : {"parent" : "LPA", "children" : []},
+}
+
+cl_params = {
+    "metadata" : { "type" : "centerline", "version" : "0.0.1"},
+    "params_domain" : {"method" : "flux"}, #This means use the default params params for flux method in centerline domain extraction method.
+    "params_path"   : { "mode"              : "j2o",
+                        "reverse"           : False,
+                        "adjacency_factor"  : 0.25,
+                        "pass_pointid"      : True},
+    "n_knots"           : 10,
+    "curvature_penalty" : 1,
+    "graft_rate"        : 0.5,
+    "force_extremes"    : True,
+    #"LPA" : {},
+    "RPA" : {"n_knots" : 5}
+}
+
+ec_params = {
+    "metadata" : { "type" : "encoding", "version" : "0.0.1"},
+    "method"            : "decoupling",
+    "tau_knots"         : 15,
+    "theta_knots"       : 15,
+    "laplacian_penalty" : 1e-2,
+    "insertion"         : 1.0,
+    #"LPA" : {},
+    "RPA" : {"tau_knots" : 5, "theta_knots" : 7}
+}
